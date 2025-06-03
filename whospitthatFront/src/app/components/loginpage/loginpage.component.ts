93b8c1345dba52470ac2d4b5bd1b348b74e2a379
Cailloux Alexandre <alexandre.cailloux@etu.univ-orleans.fr>
2025-06-03 11:06:31 +0200

    Creating basic login form

--- whospitthatFront/src/app/components/loginpage/loginpage.component.ts
+++ whospitthatFront/src/app/components/loginpage/loginpage.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {VarstorageService} from '../../services/varstorage.service';
+import {FormsModule, NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-loginpage',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './loginpage.component.html',
   styleUrl: './loginpage.component.scss'
 })
 export class LoginpageComponent {
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private varstorageService: VarstorageService) {
   }
 
   ngOnInit(){
+  }
+
+  onSubmit(form: NgForm){
+    this.authService.login(form.value.email, form.value.password).subscribe();
+    console.log(this.varstorageService.gettoken());
   }
 }
